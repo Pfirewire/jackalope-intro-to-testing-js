fc34c90542af3c47091933e0bae609324447f5c8
Steve Pflug <stephen@pflug.us>
2023-11-16 09:35:38 -0600

    completed through exercise 6

--- code.js
+++ code.js
@@ -1,4 +1,34 @@
 // helloWorld function 
-function helloWorld() {
+// function helloWorld() {
+//     return "Hello, World!";
+//     // console.log("Hello, World!");
+// }
+
+const helloWorld = () => {
     return "Hello, World!";
 }
+
+function sayHello(input) {
+    // if(input === 'Pat') {
+    //     return "Hello, Pat!";
+    // }
+    // if(input === 'Alex') {
+    //     return "Hello, Alex!";
+    // }
+    // return "Hello, Jane!";
+
+
+    // first refactor
+    return "Hello, " + input + "!";
+    // return `Hello, ${input}!`;
+}
+
+
+
+
+
+
+
+
+
+
